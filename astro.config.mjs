ed6f42bc590cee5b823a0f4f5507eef18bbce6a7
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2024-09-17 19:19:02 +0200

    Added site config parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.nonogram.pro',
   integrations: [svelte(), tailwind(), playformInline(), playformFormat(), playformCompress()]
 });
