c67b3bcb370d90256dc67925f69fabc5d48c9e02
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2024-09-21 08:32:33 +0200

    Removed @playform/format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import playformCompress from '@playform/compress';
 import playformInline from '@playform/inline';
-import playformFormat from '@playform/format';
 
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nonogram.pro',
-  integrations: [svelte(), tailwind(), playformInline(), playformFormat(), playformCompress()]
+  integrations: [svelte(), tailwind(), playformInline(), playformCompress()]
 });
